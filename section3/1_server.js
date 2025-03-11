2d607af57feae2c296773ba04efe4154fbd829fc
kimgyejeong <pmwkdo@gmail.com>
2025-03-11 15:03:09 +0900

    section3. ch11. Path 별로 다른 응답 변환하기.

--- section3/1_server.js
+++ section3/1_server.js
@@ -1,12 +1,26 @@
 const http = require('http');
+const url = require('url');
 
 const host = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res)=> {
-    // res.writeHead(statusCode, Header)
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('<h1>Hello World!</h1>');
+const server = http.createServer((req, res) => {
+    const path = url.parse(req.url).pathname;
+
+    if (path === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>HOME PAGE!</h1>');
+    } else if (path === '/post') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>POST PAGE!</h1>');
+    } else if (path === '/user') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>USER PAGE!</h1>');
+    } else {
+        // res.writeHead(statusCode, Header)
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>PAGE NOT FOUND!</h1>');
+    }
 });
 
 server.listen(port, host, () => {
