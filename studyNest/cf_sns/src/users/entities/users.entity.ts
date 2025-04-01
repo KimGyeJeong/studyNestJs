4dc7bbd406a7e1cdcac48670ea1aafa3964a498b
kimgyejeong <pmwkdo@gmail.com>
2025-04-01 10:55:19 +0900

    section11. ch66. Column 옵션 탐구

--- studyNest/cf_sns/src/users/entities/users.entity.ts
+++ studyNest/cf_sns/src/users/entities/users.entity.ts
@@ -1,16 +1,22 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
-export class UsersModel{
+export class UsersModel {
     @PrimaryGeneratedColumn()
     id: number;
-    
-    @Column()
+
+    @Column({
+        // 1) 최대길이 20
+        length: 20,
+        // 2) 유일무이한 값
+        unique: true,
+    })
     nickname: string;
-    
+
     @Column()
+        // 1) 유일무이한 값
     email: string;
-    
+
     @Column()
     password: string;
 }
