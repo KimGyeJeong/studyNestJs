1fc377edd79857dad9e4eba2214ac2c74fd27654
kimgyejeong <pmwkdo@gmail.com>
2025-05-04 01:43:09 +0900

    section21. ch117. Validation Message 일반화하기

--- studyNest/cf_sns/src/users/entities/users.entity.ts
+++ studyNest/cf_sns/src/users/entities/users.entity.ts
@@ -2,7 +2,7 @@ import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Upd
 import {RolesEnum} from "../const/roles.const";
 import {PostsModel} from "../../posts/entities/posts.entity";
 import {BaseModel} from "../../common/entities/base.entity";
-import {IsEmail, IsString, Length} from "class-validator";
+import {IsEmail, IsString, Length, ValidationArguments} from "class-validator";
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -14,7 +14,26 @@ export class UsersModel extends BaseModel {
         unique: true,
     })
     @IsString()
-    @Length(1,20, {message: '닉네임은 1에서 20자 사이로 입력해주세요.'})
+    @Length(1, 20, {
+        message(args: ValidationArguments) {
+            /**
+             * ValidationArguments의 프로퍼티들
+             * 1. value -> 검증되고있는 값(입력된 값)
+             * 2. constraints -> 파라미터에 입력된 제한 사항들
+             *  args.constraints[0] -> 1 min
+             *  args.constraints[1] -> 20 max
+             * 3. targetName -> 검증하고 있는 클래스의 이름
+             * 4. object -> 검증하고 있는 객체
+             * 5. property -> 검증 되고 있는 객체의 프로퍼티 닉네임 : nickname
+             */
+            if (args.constraints.length === 2) {
+                return `${args.property} 은 최대 ${args.constraints[0]} ~ ${args.constraints[1]} 글자를 입력해주세요.`;
+            } else {
+                return `${args.property}는 최소 ${args.constraints[0]} 글자를 입력해주세요.`;
+            }
+
+        }
+    })
     nickname: string;
 
     @Column()
@@ -25,7 +44,7 @@ export class UsersModel extends BaseModel {
 
     @Column()
     @IsString()
-    @Length(3,8)
+    @Length(3, 8)
     password: string;
 
     @Column({
