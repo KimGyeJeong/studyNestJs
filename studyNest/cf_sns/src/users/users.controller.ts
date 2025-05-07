9aea5b2cb90ca51c914962f818a9b709459e0a13
kimgyejeong <pmwkdo@gmail.com>
2025-05-07 09:27:50 +0900

    section22. ch118. Exclude Annotation

--- studyNest/cf_sns/src/users/users.controller.ts
+++ studyNest/cf_sns/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, ClassSerializerInterceptor, Controller, Get, Post, UseInterceptors} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -13,6 +13,14 @@ export class UsersController {
   // }
   
   @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
+  /**
+   * Serialization --> 직렬화
+   * 현재 시스템(nestJS)에서 사용되는 데이터의 구조를 다른 시스템에서도 쉽게 사용할 수 있는 포멧으로 변환
+   *  --> Class의 Object를 JSON 포멧으로 변환
+   *  
+   * DeSerialization --> 역직렬화
+   */
   getUsers(){
     return this.usersService.getAllUsers();
   }
