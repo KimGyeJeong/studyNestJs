3c08e75da26fa8a574277e5b1db911e423093ca7
kimgyejeong <pmwkdo@gmail.com>
2025-05-27 10:33:52 +0900

    section26. ch150. process 객체를 이용해서 환경변수 불러오기. - configModule 을 먼저 import 하지 않는다면 OS의 env를 기본적으로 가지고오는 문제가 있음.

--- studyNest/cf_sns/src/app.module.ts
+++ studyNest/cf_sns/src/app.module.ts
@@ -10,22 +10,31 @@ import {AuthModule} from './auth/auth.module';
 import {CommonModule} from './common/common.module';
 import {APP_INTERCEPTOR} from "@nestjs/core";
 import {ConfigModule} from "@nestjs/config";
+import * as process from "node:process";
+import {
+    ENV_DB_DATABASE_KEY,
+    ENV_DB_HOST_KEY,
+    ENV_DB_PASSWORD_KEY,
+    ENV_DB_PORT_KEY,
+    ENV_DB_USERNAME_KEY
+} from "./common/const/env-keys.const";
 
 @Module({
-    imports: [PostsModule, TypeOrmModule.forRoot({
+    imports: [PostsModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),
+        TypeOrmModule.forRoot({
         type: 'postgres', // 데이터베이스 타입
-        host: 'localhost',// db 주소
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
+        host: process.env[ENV_DB_HOST_KEY],// db 주소
+        port: parseInt(process.env[ENV_DB_PORT_KEY] ?? '5432'),
+        username: process.env[ENV_DB_USERNAME_KEY],
+        password: String(process.env[ENV_DB_PASSWORD_KEY]),
+        database: process.env[ENV_DB_DATABASE_KEY],
         entities: [
             PostsModel,
             UsersModel
         ], // db 연결될 모델들
         synchronize: true,  // nestjs와 db의 데이터구조 sync맞쳐줌. 개발환경에서는 true가 좋지만 실제환경에서는 false로 해주어야함. 무슨타입이 올지 모르기때문.
 
-    }), UsersModule, AuthModule, CommonModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),],
+    }), UsersModule, AuthModule, CommonModule, ],
     controllers: [AppController],
     providers: [AppService, {provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor}],
 })
