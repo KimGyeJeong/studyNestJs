77304a1096b858506e282c245b501da69a3ac128
kimgyejeong <pmwkdo@gmail.com>
2025-05-29 10:24:43 +0900

    section28. ch153. Static File Serving 옵션 추가하기.

--- studyNest/cf_sns/src/app.module.ts
+++ studyNest/cf_sns/src/app.module.ts
@@ -18,9 +18,11 @@ import {
     ENV_DB_PORT_KEY,
     ENV_DB_USERNAME_KEY
 } from "./common/const/env-keys.const";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {PUBLIC_FOLDER_PATH} from "./common/const/path.const";
 
 @Module({
-    imports: [PostsModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),
+    imports: [PostsModule, ServeStaticModule.forRoot({rootPath:PUBLIC_FOLDER_PATH, serveRoot:'/public'}), ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),
         TypeOrmModule.forRoot({
         type: 'postgres', // 데이터베이스 타입
         host: process.env[ENV_DB_HOST_KEY],// db 주소
