ec261560d1e554c1564bc1d3381ce2b327129fd1
KimGyeJeong <pmwkdo@gmail.com>
2025-06-14 02:55:59 +0900

    section34. ch175. 서버에서 이벤트 전송하기.

--- studyNest/cf_sns/src/chats/chats.gateway.ts
+++ studyNest/cf_sns/src/chats/chats.gateway.ts
@@ -1,18 +1,41 @@
-import {MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway} from "@nestjs/websockets";
-import {Socket} from "socket.io";
+import {
+    ConnectedSocket,
+    MessageBody,
+    OnGatewayConnection,
+    SubscribeMessage,
+    WebSocketGateway, WebSocketServer
+} from "@nestjs/websockets";
+import {Socket, Server} from "socket.io";
 
 @WebSocketGateway({
     // ws://localhost:3000/chats
     namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection{
+    @WebSocketServer()
+    server: Server;
+    
     handleConnection(socket: Socket,) {
         console.log(`on Connect Called : ${socket.id}`);
     }
     
     // socket.on("send_message", (message) => {console.log(message) } );
     @SubscribeMessage('send_message')
-    sendMessage(@MessageBody() message:string){
-        console.log(message);
+    sendMessage(@MessageBody() message: { message: string, chatId: number},
+                @ConnectedSocket() socket: Socket
+                ){
+        this.server.emit('receive_message', 'hello from server');
+    }
+    
+    @SubscribeMessage('enter_chat')
+    enterChat(
+        // 방의 ID들을 리스트로 받음
+        @MessageBody() data: number[],
+        @ConnectedSocket() socket: Socket,
+        ){
+            for( const chatId of data){
+                // socket.join()
+                socket.join(chatId.toString());
+            }
     }
 }
