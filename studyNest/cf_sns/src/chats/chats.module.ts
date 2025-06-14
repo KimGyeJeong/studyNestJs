8b8b9d3c65124a0b767795786d103116080a8636
KimGyeJeong <pmwkdo@gmail.com>
2025-06-14 14:03:11 +0900

    section34. ch179. Paginate Chat API 생성하기.

--- studyNest/cf_sns/src/chats/chats.module.ts
+++ studyNest/cf_sns/src/chats/chats.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ChatsService } from './chats.service';
-import { ChatsController } from './chats.controller';
+import {Module} from '@nestjs/common';
+import {ChatsService} from './chats.service';
+import {ChatsController} from './chats.controller';
 import {ChatsGateway} from "./chats.gateway";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ChatsModel} from "./entity/chats.entity";
+import {CommonModule} from "../common/common.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatsModel])],
-  controllers: [ChatsController],
-  providers: [ChatsService, ChatsGateway],
+    imports: [TypeOrmModule.forFeature([ChatsModel]), CommonModule],
+    controllers: [ChatsController],
+    providers: [ChatsService, ChatsGateway],
 })
-export class ChatsModule {}
+export class ChatsModule {
+}
