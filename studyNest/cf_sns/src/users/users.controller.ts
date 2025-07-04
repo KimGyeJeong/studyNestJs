13196de3b5dac572002cbfb4a04dbe2c5ca76978
kimgyejeong <pmwkdo@gmail.com>
2025-07-04 11:39:27 +0900

    section39. ch237. Follow 시스템 로직 작성하고 테스트 하기.

--- studyNest/cf_sns/src/users/users.controller.ts
+++ studyNest/cf_sns/src/users/users.controller.ts
@@ -1,31 +1,60 @@
-import {Body, ClassSerializerInterceptor, Controller, Get, Post, UseInterceptors} from '@nestjs/common';
-import { UsersService } from './users.service';
+import {
+    Body,
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    UseInterceptors
+} from '@nestjs/common';
+import {UsersService} from './users.service';
 import {RolesEnum} from "./const/roles.const";
 import {Roles} from "./decorator/roles.decorator";
+import {User} from "./decorator/user.decorator";
+import {UsersModel} from "./entity/users.entity";
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-  
-  // @Post()
-  // postUser(@Body('nickname') nickname: string, 
-  //          @Body('email') email: string, 
-  //          @Body('password') password: string) {
-  //   return this.usersService.createUser({nickname, email, password});
-  // }
-  
-  @Get()
-  @Roles(RolesEnum.ADMIN)
-  // appModule에서 사용
-  // @UseInterceptors(ClassSerializerInterceptor)
-  /**
-   * Serialization --> 직렬화
-   * 현재 시스템(nestJS)에서 사용되는 데이터의 구조를 다른 시스템에서도 쉽게 사용할 수 있는 포멧으로 변환
-   *  --> Class의 Object를 JSON 포멧으로 변환
-   *  
-   * DeSerialization --> 역직렬화
-   */
-  getUsers(){
-    return this.usersService.getAllUsers();
-  }
+    constructor(private readonly usersService: UsersService) {
+    }
+
+    // @Post()
+    // postUser(@Body('nickname') nickname: string, 
+    //          @Body('email') email: string, 
+    //          @Body('password') password: string) {
+    //   return this.usersService.createUser({nickname, email, password});
+    // }
+
+    @Get()
+    @Roles(RolesEnum.ADMIN)
+    // appModule에서 사용
+    // @UseInterceptors(ClassSerializerInterceptor)
+    /**
+     * Serialization --> 직렬화
+     * 현재 시스템(nestJS)에서 사용되는 데이터의 구조를 다른 시스템에서도 쉽게 사용할 수 있는 포멧으로 변환
+     *  --> Class의 Object를 JSON 포멧으로 변환
+     *
+     * DeSerialization --> 역직렬화
+     */
+    getUsers() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Post('follow/:id')
+    async postFollow(
+        @User() user: UsersModel,
+        @Param('id', ParseIntPipe) followeeId: number,
+    ) {
+        await this.usersService.followUser(user.id, followeeId);
+
+        return true;
+    }
+
+    @Get('follow/me')
+    async getFollow(
+        @User() user: UsersModel,
+    ) {
+        return this.usersService.getFollowers(user.id);
+    }
 }
--- studyNest/cf_sns/src/users/users.service.ts
+++ studyNest/cf_sns/src/users/users.service.ts
@@ -8,43 +8,84 @@ export class UsersService {
     constructor(
         @InjectRepository(UsersModel)
         private readonly usersRepository: Repository<UsersModel>,
-    ){    }
+    ) {
+    }
 
-    async createUser(user : Pick<UsersModel, 'email'|'nickname'|'password'>){
+    async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
         // 1. 닉네임 중복 확인. exists() 사용. 값이있으면 true 반환
         const isExistsNickName = await this.usersRepository.exists({
             where: {nickname: user.nickname},
         })
-        if (isExistsNickName){
+        if (isExistsNickName) {
             throw new BadRequestException('Nickname already exists');
         }
         // 2. 이메일 중복 확인.
         const isExistsEmail = await this.usersRepository.exists({
             where: {email: user.email},
         });
-        if (isExistsEmail){
+        if (isExistsEmail) {
             throw new BadRequestException('Email already exists');
         }
         const userObject = this.usersRepository.create({
-            nickname : user.nickname,
-            email : user.email, 
-            password : user.password,
+            nickname: user.nickname,
+            email: user.email,
+            password: user.password,
         });
-        
+
         const savedUser = await this.usersRepository.save(user);
-        
+
         return savedUser;
     }
-    
-    async getAllUsers(){
+
+    async getAllUsers() {
         return await this.usersRepository.find();
     }
-    
-    async getUserByEmail(email : string){
+
+    async getUserByEmail(email: string) {
         return this.usersRepository.findOne({
             where: {
                 email,
             },
         });
+    }
+
+    async followUser(followerId: number, followeeId: number) {
+        const user = await this.usersRepository.findOne({
+            where: {
+                id: followerId,
+            },
+            relations: {
+                followees: true,
+            },
+        });
+
+        if (!user) {
+            throw new BadRequestException(`follower ${followerId} not found}`);
+        }
+
+        await this.usersRepository.save({
+            ...user,
+            followees: [
+                ...user.followees,
+                {id: followeeId}
+            ]
+        })
+    }
+    
+    async getFollowers(userId: number) : Promise<UsersModel[]>{
+        const user = await this.usersRepository.findOne({
+            where: {
+                id: userId,
+            },
+            relations: {
+                followers: true,
+            }
+        });
+        
+        if (!user) {
+            throw new BadRequestException(`follower ${userId} not found}`);
+        }
+
+        return user.followers;
     }
 }
